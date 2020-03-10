7814483852fe1cd32d6a8fe1df7c09591a237020
sengahk <58158957+sengahk@users.noreply.github.com>
2020-03-10 09:51:52 +0700

    first project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,104 @@
+
+<!DOCTYPE html>
+	<html>
+	<head>
+	<script src="js/app.js"></script>
+    <link href="css/app.css" rel="stylesheet" type="text/css">
+  
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>Test BootStrap4</title>
+	</head>
+	<body>
+		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+			<a class="navbar-brand" href="#">Responsive</a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarSupportedContent">
+				<ul class="navbar-nav mr-auto">
+					 <li class="nav-item active">
+						 <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+					 </li>
+					 <li class="nav-item dropdown">
+						 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						 Program
+						 </a>
+						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
+							  <a class="dropdown-item" href="#">DUT</a>
+							  <a class="dropdown-item" href="#">Engineer</a>
+							  <a class="dropdown-item" href="#">Master</a>
+						</div>
+					 </li>
+					  <li class="nav-item">
+						<a class="nav-link disabled" href="#">About Us</a>
+					  </li>
+					  <li class="nav-item">
+						<a class="nav-link disabled" href="#">Contact Us</a>
+					  </li>
+				</ul>
+				<form class="form-inline my-2 my-lg-0">
+					  <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+					  <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+				 </form>
+		   </div>
+		</nav>
+		  
+		<div class="jumbotron">
+		<div class="container text-center">
+		<h1>Home Page Responsive</h1>
+		<p>
+			This is a template for a simple marketing or informational
+			website. It includes a large callout called the hero unit
+			and three supporting pieces of content. Use it as a
+			starting point to create something more unique.
+		</p>
+		  <a class="btn btn-success btn-lg-center " href="#" >
+			Free Trail»
+		  </a>
+		   <a class="btn btn-info btn-lg-center" href="#">
+			View our services »
+		  </a>
+		</div>
+	  </div>
+	  <div class="container">
+		<div class="row">
+		  <div class="col-sm " >
+			<div class="border border-primary text-center rounded">
+			  <i class="fas fa-globe-americas">
+			  </i>
+			<p>Earth</p>
+			</div>
+			 
+		  </div>
+		  <div class="col-sm " >
+			<div class="border border-primary text-center rounded">
+			  <i class="fab fa-apple"></i>
+			  <p>Apple</p>
+			</div>
+			 
+		  </div>
+		  <div class="col-sm " >
+			<div class="border border-primary text-center rounded">
+			  <i class="fas fa-charging-station"></i>
+			  <p>Charging-station</p>
+			</div>
+			 
+		  </div>
+		  <div class="col-sm " >
+			<div class="border border-primary text-center rounded">
+			  <i class="far fa-building"></i>
+			  <p>building</p>
+			</div>
+		  </div>
+		</div>
+		<div class="footer">
+		  2020lysivheng
+		</div>
+	  </div>
+	  
+		
+		 
+	  
+	  </body>
+</html>	
